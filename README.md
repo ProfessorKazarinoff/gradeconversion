ff1a456afd388a7510e08e7f054f6f8475468e31
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-05-23 11:59:43 -0700

    run.py and README updates

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # gradeconversion
-A python script to convert grades from the WileyPlus.com online homework tool to the D2L learning management system 
+A Python script to convert grades from the WileyPlus.com online homework tool to the D2L learning management system. Uses a great python package called [**gooey**](https://github.com/chriskiehl/Gooey) to produce a GUI that runs the script.
 
-#### Dependancies
+## Installation
+
+Assumes you have the [Anaconda distribution of Python](https://www.anaconda.com/download/) installed and are working from the Anaconda Prompt.
+
 ```
-pip install pandas
-pip install fuzzywuzzy
-pip install gooey
+# clone the repo
+
+$ git clone https://github.com/ProfessorKazarinoff/gradeconversion.git
+
+# create the conda environment
+$ cd gradeconversion
+$ conda env create -f environment.yml
+```
+
+## Running the Script
+
+Assumes you have the Anaconda distribution of Python installed and are working from the Anaconda Prompt and are currently in the  ```gradeconversion directory```
+
+```
+$ conda activate gradeconversion
+(gradeconversion)$ python run.pyu
 ```
---
+++ run.py
@@ -0,0 +1,112 @@
+#! /usr/bin/env python
+
+import pandas as pd
+from D2Ltools import extract_D2L_assigns, extract_D2L_usernames
+from D2Ltools import extract_D2L_OrgIDs
+from wptools import extract_wp_assigns, extract_wp_names
+from fuzzytools import match_list
+from gooey import Gooey
+from gooey import GooeyParser
+import os
+
+def convert_grades(wp='wp_download.csv', D2L='D2L_download.csv'):
+    """
+    This function takes in a WileyPlus .csv download of student grades along with a download of D2L grades
+    It then outputs a pandas dataframe in the format that D2L can read.
+    output dataframe needs to be converted to a .csv before uploading to D2L
+    :param wp: .csv download from WileyPlus. Full file path including .csv extension
+    :param D2L: .csv download from D2L. Full file path including .csv extension
+    :return: df, pandas dataframe formatted for D2L
+    """
+    wp_csv = wp
+    D2L_csv = D2L
+
+    # read in D2L csv as pandas df, make a list of D2L Usernames
+    df2 = pd.read_csv(D2L_csv)
+    d2l_username_lst = df2['Username'].tolist()
+
+    # read in WileyPlus csv as pandas df, skip last line which has totals instead of a student name
+    # Insert a new column that combines First and Last names to make one name with a space
+    # make a list of these WileyPlus full names
+    df = pd.read_csv(wp_csv, skipinitialspace=True, skipfooter=1, engine='python')
+    df.insert(0, 'wp_name', df['First Name'] + ' ' + df['Last Name'])
+    wp_name_lst = df['wp_name'].tolist()
+
+    # make a dict with {Keys: 'WileyPlus Full Names', Values:'D2L Usernames'}
+    wpname2d2lusername_dict = match_list(wp_name_lst, d2l_username_lst)
+
+    # insert a new column in WileyPlus df with D2L Usernames mapped from WileyPlus full names
+    # using dict with {Keys: 'WileyPlus Full Names', Values:'D2L Usernames'}
+    df.insert(0, 'Username', df['wp_name'].map(wpname2d2lusername_dict))
+
+    # make a dict of {Keys: 'D2L Usernames', Values: 'D2L OrgDefinedIds'}
+    d2l_username_to_orgid_dict = dict(zip(df2['Username'], df2['OrgDefinedId']))
+    # insert in a new first column in WileyPlus df with D2L OrgDefinedIds mapped from D2L Usernames
+    # using dict with {Keys: 'D2L Usernames', Values: 'D2L OrgDefinedIds'}
+    df.insert(0, 'OrgDefinedId', df['Username'].map(d2l_username_to_orgid_dict))
+
+    # remove hash(#) signs, drop columns that aren't grades, drop columns with stars(*)
+    df['OrgDefinedId'] = df['OrgDefinedId'].map(lambda x: x.lstrip('#'))
+    df['Username'] = df['Username'].map(lambda x: x.lstrip('#'))
+    cols_to_drop_lst = ['wp_name', 'First Name', 'Last Name', 'Email', 'Course name', 'Class-section name', 'Date', 'Overall Score (%)']
+    df.drop(cols_to_drop_lst, axis=1, inplace=True)
+    col_name_lst = df.columns.values.tolist()
+    cols_with_star_lst = [c for c in col_name_lst if '*' in c]
+    df.drop(cols_with_star_lst, axis=1, inplace=True)
+
+    # get list of WileyPlus and D2L assignments, them match them up in a
+    # dict of {Keys: 'WileyPlus Assigns': Values:'D2L Assigns'}
+    d2l_assings_lst = extract_D2L_assigns(D2L_csv)
+    wp_assigns_lst = extract_wp_assigns(wp_csv)
+    wpassigns2d2lassigns_dict = match_list(wp_assigns_lst, d2l_assings_lst)
+
+    # use dict of WileyPlus and D2L Assigns to rename columns with D2L assign names
+    df.rename(columns=wpassigns2d2lassigns_dict, inplace=True)
+
+    # Take out blanks, Grades not released and Not assigned entries
+    df = df.replace('Grades Not Released', ' ', regex=True)  # remove all Grades not replaced cells
+    df = df.replace('Not Assigned', ' ', regex=True)  # remove all not assigned marks
+    df = df.fillna('')
+    df['End-of-Line Indicator'] = '#'  # add a column with End-of-Line Indicator # needed by D2L
+
+    df.set_index('OrgDefinedId', drop=True, inplace=True) # set first column as D2L OrgDefinedId's
+
+    #Now the dataframe should be ready to go. Just print out to make sure.
+    print(df)
+    return(df)
+
+@Gooey
+def main(wp='wp_download.csv', D2L='D2L_download.csv', output='output.csv'):
+    parser = GooeyParser(description="Grade Conversion GUI App! \n Download WileyPlus and D2L .csv grade exports \n Remove teacher rows from D2L .csv before import")
+    parser.add_argument('WileyPlus_csv',
+                        help="Browse to the WileyPlus file you downloaded",
+                        widget='FileChooser')
+    parser.add_argument('D2L_csv',
+                        help="Browse to the D2L file you downloaded",
+                        widget='FileChooser')
+    parser.add_argument('output_dir',
+                        help="Choose the output directory you want to dump your output .csv file into",
+                        widget='DirChooser')
+    parser.add_argument('output_csv',
+                        help="Type name of output csv file, include .csv extension",
+                        widget='TextField',)
+    args = vars(parser.parse_args())
+
+    wp_csv = (args["WileyPlus_csv"])
+    D2L_csv = (args["D2L_csv"])
+    output_dir = (args["output_dir"])
+    output_csv = (args["output_csv"])
+
+    # big function that creates the pandas dataframe in the D2L grade format with all the WileyPlus Grades
+    out_df = convert_grades(wp_csv, D2L_csv)
+    print('\n Output dataframe created successfully \n ')
+
+    # write the output dataframe to  csv
+    out_file_path = os.path.join(output_dir, output_csv)
+    out_df.to_csv(out_file_path)
+
+    print(f' \n \n Success! Output file \n \n {output_csv} \n \n is saved in \n \n {output_dir}')
+
+
+if __name__ == "__main__":
+    main()
